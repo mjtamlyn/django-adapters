68aa53a3375e249cfb657894bdf671d5fa97edcf
Marc Tamlyn <marc.tamlyn@gmail.com>
2016-04-03 09:40:47 +0200

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(
+    name='django-serial-forms',
+    py_modules=['serial_forms'],
+    version='0.1',
+    description='Constructible, introspectable serializers and forms',
+    author='Marc Tamlyn & Friends',
+    author_email='marc.tamlyn@gmail.com',
+    url='https://github.com/mjtamlyn/django-serial-forms',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
